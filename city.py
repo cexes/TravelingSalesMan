af5c75d6139444774928a499d369bd1add41bed9
cexes <mottacesar001@gmail.com>
2023-05-23 02:25:05 -0300

    Refactor

--- city.py
+++ city.py
@@ -1,35 +1,39 @@
-import requests
-"""
-Módulo que contém a definição da classe City.
-"""
+import math
 
 class City:
-    """
-    Classe que representa uma cidade.
-    """
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.visited = False
 
-    def __init__(self, citys):
-        """
-        Inicializa uma instância da classe City.
+def calculate_distance(city1, city2):
+    lat1, lon1 = city1.latitude, city1.longitude
+    lat2, lon2 = city2.latitude, city2.longitude
+    distance = math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2)
+    return distance
 
-        Args:
-            city (str): O nome da cidade.
-            latitude (float): A latitude da cidade.
-            longitude (float): A longitude da cidade.
-        """
-        self.citys = citys
-        print(self.citys)
+def TSP_Nearest_Neighbor(cities, adjacency_matrix):
+    initial_city = cities[0]
+    initial_city.visited = True
+    path = [initial_city]
+    total_cost = 0
 
-    def get_longitude_latitude(self):
-        i = 0
-        while i < len(self.citys):
-          url =f"https://nominatim.openstreetmap.org/search?format=json&q={self.citys[i]}"
-          response = requests.get(url)
-          data = response.json()
-          latitude = data[0]['lat']
-          longitude = data[0]['lon']
-          print(self.citys[i],'latidude: ',latitude," ",'longitude: ', longitude)
-          i +=1
-way = ['acre', 'bahia','amazonas']
-city = City(way)        
-print(city.get_longitude_latitude())
+    while len(path) < len(cities):
+        current_city = path[-1]
+        nearest_city = None
+        min_distance = float('inf')
+
+        for adjacent_city in adjacency_matrix[current_city]:
+            if not adjacent_city.visited and adjacency_matrix[current_city][adjacent_city] < min_distance:
+                nearest_city = adjacent_city
+                min_distance = adjacency_matrix[current_city][adjacent_city]
+
+        path.append(nearest_city)
+        nearest_city.visited = True
+        total_cost += min_distance
+
+    total_cost += adjacency_matrix[path[-1]][initial_city]
+    path.append(initial_city)
+
+    return total_cost, path
