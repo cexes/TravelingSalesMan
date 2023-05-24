e6813df7d4c8ed9ceef35619c9d524417a1fffb8
cexes <mottacesar001@gmail.com>
2023-05-24 14:27:09 -0300

    NO-HARDCODE

--- city.py
+++ city.py
@@ -7,3 +7,20 @@ class City:
         self.latitude = latitude
         self.longitude = longitude
         print(self.name, self.latitude,self.longitude)
+
+    def calculate_distancies(self,citie1,citie2):
+        distance = math.sqrt((citie2[0] - citie1[0]) ** 2 + (citie2[1] - citie1[1]) ** 2)
+        return distance
+
+    def calculate_adj(cities):
+         num_cities = len(cities)
+         adj_matriz = [[0] * num_cities for _ in range(num_cities)]
+
+         for i in range(num_cities):
+             for j in range(num_cities):
+                if i != j:
+                  distance = calculate_distancies(cities[i], cities[j])
+                  adj_matrix[i][j] = distance
+
+         return adj_matrix    
+
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import requests
 class main:
 
    cities = [ "Acre" , "Bahia ","Sergipe"]
-
+   lat = []
+   lon = []
 
    for city in cities:
        url = f"https://nominatim.openstreetmap.org/search?format=json&q={city}"
        response = requests.get(url)
        data = response.json()
-       latitude = data[0]['lat'] 
-       longitude = data[0]['lon']
+       latitude = float(data[0]['lat']) 
+       lat.append(latitude)
+       longitude = float(data[0]['lon'])
+       lon.append(longitude)
 
    for i in range(len(cities)):
-       city = City(cities[i],latitude,longitude)        
+       c = City(cities[i],lat[i],lon[i])
+  
